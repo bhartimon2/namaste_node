b5e5aca27540e09ec39673317c6cffe9e7c1d4cd
bhartimon2 <36557316+bhartimon2@users.noreply.github.com>
2026-03-16 13:21:33 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require("mongodb");
+
+
+
+const url =
+"mongodb+srv://bhartimontu231_db_user:nykqiGtvJPVhMzNb@namastenode.zoojjo4.mongodb.net/?appName=NamasteNode";
+
+const client = new MongoClient(url);
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+}
+
+main().catch(console.error);
